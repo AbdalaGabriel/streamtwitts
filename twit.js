12e59745a8ec55588caef8eaf57f6ad9e6efd3d9
AbdalaGabriel <g.abdala.04@gmail.com>
2017-08-22 00:11:49 -0300

    v3
    Version final  - 2leds 2hash

--- twit.js
+++ twit.js
@@ -1,4 +1,27 @@
-console.log("Twitting");
+console.log("hola");
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+ 
+ var led2 = new five.Led(8);
+ var led = new five.Led(13);
+
+// led.brightness(256);
+// led2.brightness(256);
+
+  // This will grant access to the led instance
+  // from within the REPL that's created when
+  // running this program.
+  this.repl.inject({
+    led: led,
+    led2: led2,
+  });
+
+
+
+
+console.log("Twitting2");
 var Twitter = require('node-tweet-stream')
   , t = new Twitter({
     consumer_key: 'Q8DZ6nk8NG50cRjNWwQtjAIJ7',
@@ -7,45 +30,53 @@ var Twitter = require('node-tweet-stream')
     token_secret: '83Zj1bobqBqXFIDknSsoLyK1i0FyERpSi8b8H4Z35APre'
   })
 
-var five = require("johnny-five");
-var board = new five.Board();
-
-board.on("ready", function() {
-  var led = new five.Led(13);
-
-  // This will grant access to the led instance
-  // from within the REPL that's created when
-  // running this program.
-  this.repl.inject({
-    led: led
-  });
+var Twitter2 = require('node-tweet-stream')
+  , t2 = new Twitter({
+    consumer_key: 'Q8DZ6nk8NG50cRjNWwQtjAIJ7',
+    consumer_secret: 'ZSp8I8aQ27FQ4I8DjQPjNZYoZpVxxFg2Uwl6ygibQpcXnbKjdS',
+    token: '796018368100888576-VxLJbv2peY7nU802GnGiuAoEfBmlUZp',
+    token_secret: '83Zj1bobqBqXFIDknSsoLyK1i0FyERpSi8b8H4Z35APre'
+  })
 
 
-
-
-var i = 0;
+var j = 0;
+var g = 0;
 t.on('tweet', function (tweet) {
-  console.log("----------------------------");
+  console.log("--------------LOVE--------------");
   console.log(tweet.text);
-  console.log(i);
-  i++;
+  console.log(j);
+  j++;
   console.log("----------------------------");
-
-  led.blink(100, stop);
-
-
-  function stop(){
-    console.log("led");
-    led.stop();
-    led.off();
-  }
-
+  led.blink(200, stop);
 })
+
+t2.on('tweet', function (tweet) {
+  console.log("--------------GOT--------------");
+  console.log(tweet.text);
+  console.log(g);
+  g++;
+  console.log("----------------------------");
+  led2.blink(200, stop2);
+})
+
+
+function stop(){
+  console.log("stop");
+  led.stop();
+  led.off();
+}
+
+function stop2(){
+  console.log("stop2");
+  led2.stop();
+  led2.off();
+}
+
 
 t.on('error', function (err) {
   console.log('Oh no')
-})
-
-t.track('#AnimalesSueltos');
+});
+t2.track('#got');
+t.track('#love');
 
 });
